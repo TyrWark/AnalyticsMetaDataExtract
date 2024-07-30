7db1c8d31d341e3785745bab94696cb5f415c1f1
TyrWark <167569189+TyrWark@users.noreply.github.com>
2024-07-30 12:51:19 -0500

    Create tamper.js

---
+++ tamper.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         FieldsFromReport
+// @namespace    http://tampermonkey.net/
+// @version      2024-07-24
+// @description  try to take over the world!
+// @author       Ty Wark
+// @match        https://lightspeedanalytics.net/cl_accounts/settings
+// @match        https://app.lightspeedanalytics.net/embed/explore/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=zendesk.com
+// @require      https://raw.githubusercontent.com/TyrWark/AnalyticsMetaDataExtract/main/include.js
+// @grant      GM_registerMenuCommand
+// ==/UserScript==
+
+
+
+console.log("Fired MetaData")
